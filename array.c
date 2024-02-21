4322d3c680a1380f7c9aa3922c986854cc17e9bf
Shampy <157746114+codershampy@users.noreply.github.com>
2024-02-22 00:02:05 +0530

    Update array.c

--- array.c
+++ array.c
@@ -4,7 +4,7 @@ void main(){
     int i,j;
     printf("Enter array elements:");
     for(i=0;i<4;i++){
-        scanf("%d",arr[i]);
+        scanf("%d",&arr[i]);
     }
     printf("Printing array element");
     for(j=0;j<4;j++){
