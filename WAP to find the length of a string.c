559c4f9874d32f7f73c5485fdea85a59d533d62d
Shampy <157746114+codershampy@users.noreply.github.com>
2024-03-26 22:06:55 +0530

    Add files via upload

---
+++ WAP to find the length of a string.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char name [20]="Programming";
+    int length;
+    length=strlen(name);
+    printf("Length of %s=%d\n",name,length);
+    return 0;
+}
